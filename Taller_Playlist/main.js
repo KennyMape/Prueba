91960c6171aa5d6c15265bc48224ffa99dfaeee8
Kenny <88409371+KennyMape@users.noreply.github.com>
2022-03-30 20:31:42 -0500

    JavaScript

---
+++ Taller_Playlist/main.js
@@ -0,0 +1,40 @@
+//---------------------------------------------------------------------------------------------------------------//
+
+var Canciones = ["Almas Gemelas", "Lova", "solo tu", "extrañandote", "poeta"];
+
+function lista(musica){
+    var arreglo = musica
+    for (var i = 0; i <arreglo.length; i++){
+        console.log(arreglo[i]);
+    }
+}
+console.log(`Mi lista de canciones preferidas: `)
+lista(Canciones)
+
+//---------------------------------------------------------------------------------------------------------------//
+
+// var Canciones = ["Almas Gemelas", "Lova", "solo tu", "extrañandote", "poeta"];
+
+// function lista(musica){
+// var arreglo = musica
+// for (var i = 0; i < arreglo.length; i++){
+// console.log(arreglo[i]);
+// }
+// }
+// console.log(`Mi lista de canciones preferidas: `)
+// lista(Canciones)
+
+//-------------------------------------------------------------------------------------------------------------//
+
+//DOMINA los CICLOS O BUÇLES EN JAVASCRIP
+
+// const miArray = ["Almas Gemelas", "Lova", "solo tu", "extrañandote", "poeta"];
+// miArray.forEach (function(item, index) {
+
+//     console.log("El valor de la posición " +index+ " es: " +item)
+// })
+
+// const miArray = ["Almas Gemelas", "Lova", "solo tu", "extrañandote", "poeta"];
+// miArray.forEach (function(item) {
+//     console.log(`Mi lista de canciones preferidas : ${item}`)
+// })
